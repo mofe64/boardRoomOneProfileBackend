483e594a388cb00adb69b3f4742c749cbc6fb048
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-08-18 19:12:45 +0100

    base route

--- src/main/java/com/example/boardroomoneprofiles/BoardRoomOneProfilesApplication.java
+++ src/main/java/com/example/boardroomoneprofiles/BoardRoomOneProfilesApplication.java
@@ -2,9 +2,16 @@ package com.example.boardroomoneprofiles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BoardRoomOneProfilesApplication {
+    @GetMapping("")
+    public String startUp(){
+        return "Backend serve up and running .... please view read me at https://github.com/mofe64/boardRoomOneProfileBackend/blob/main/README.md";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BoardRoomOneProfilesApplication.class, args);
